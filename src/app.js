6c25ac118cad84d3d3c654e7672c6c2759e23b31
Anton Tumilovich <anton_tumilovich@nevicom.ru>
2014-09-26 23:59:43 +0400

    Data over internet

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 var UI = require('ui');
+var sens_card = new UI.Card({title: 'Sensors :', icon: 'images/temp.png', body: 'Room Temp : XX'});
+var ajax = require('ajax');
 
-  var sens_card = new UI.Card({
-    title: 'Sensors :',
-    icon: 'images/temp.png',
-    body: 'Room Temp : XX'
-  });
-
-      var ajax = require('ajax');
-      ajax({ url: 'http://ictrl.home:1110/jva7' },
-
-        function(data) {
-            sens_card.body(data);
-        }
-      );
+ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva7' },
+  function(data)
+    {
+      sens_card.body(data);
+    }
+  );
  
-        sens_card.show();
+sens_card.show();
