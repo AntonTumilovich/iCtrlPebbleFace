c6f103c09544c6fcce4ff453cc9e7eb118821838
Anton Tumilovich <anton_tumilovich@nevicom.ru>
2014-09-27 22:01:06 +0400

    Local & Internet json request

--- src/app.js
+++ src/app.js
@@ -8,19 +8,19 @@ var date_data = new UI.TimeText ({position: new Vector2(25, 25), size: new Vecto
 var time_data = new UI.TimeText ({position: new Vector2(45, -4), size: new Vector2(144, 168), font: 'gothic-28-bold', text: '%H:%M'}); wind.add(time_data);
 var sens_data = new UI.Text     ({position: new Vector2(10, 60), size: new Vector2(144, 168), font: 'gothic-18', text: ''}); wind.add(sens_data);
 
-Accel.on('tap', function(e) {
-  refresh_sensors();
-  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
-});
+Accel.on('tap', function(e) {refresh_sensors();});
 
 
 function refresh_sensors(e)
 {
-  ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json' },
-    function(data) {sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + "/" + data.door_out);}
+  ajax(
+    {url: 'http://ictrl.home:1110/jva6', type: 'json' },
+    function(data) {sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + "/" + data.door_out);},
+    function() {ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json' }, function(data) {sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + "/" + data.door_out);});}
   );
 }
 
-wind.show();
 
-refresh_sensors();
+
+
+wind.show(); refresh_sensors();
