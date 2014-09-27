60d591ec50c02e04173f273904a1114594ab3fba
Anton Tumilovich <anton_tumilovich@nevicom.ru>
2014-09-27 19:01:32 +0400

    few changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
-var wind = new UI.Window();
+var wind = new UI.Window({action: {backgroundColor : 'black'}});
 var ajax = require('ajax');
-var Accel = require('ui/accel');
+var Accel = require('ui/accel'); Accel.init();
 
-Accel.init();
+var date_data = new UI.TimeText ({position: new Vector2(25, 25), size: new Vector2(144, 168), font: 'gothic-28', text: '%d/%m %a'}); wind.add(date_data);
+var time_data = new UI.TimeText ({position: new Vector2(45, -4), size: new Vector2(144, 168), font: 'gothic-28-bold', text: '%H:%M'}); wind.add(time_data);
+var sens_data = new UI.Text     ({position: new Vector2(10, 60), size: new Vector2(144, 168), font: 'gothic-18', text: ''}); wind.add(sens_data);
 
 Accel.on('tap', function(e) {
   refresh_sensors();
@@ -12,49 +14,13 @@ Accel.on('tap', function(e) {
 });
 
 
-
-// font: 'gothic-14-bold',
-
-var date_data = new UI.TimeText ({
- position: new Vector2(25, 25),
- size: new Vector2(144, 168),
- font: 'gothic-28'
-});
-
-var time_data = new UI.TimeText ({
- position: new Vector2(45, -4),
- size: new Vector2(144, 168),
- font: 'gothic-28-bold'
-});
-
-var sens_data = new UI.Text({
- position: new Vector2(10, 60),
- size: new Vector2(144, 168),
- font: 'gothic-18'
-});
-
-
-
-
-
-
-function refresh_sensors(e){
-ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json' },
-  function(data)
-    {
-      sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + "/" + data.door_out);
-    }
+function refresh_sensors(e)
+{
+  ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json' },
+    function(data) {sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + "/" + data.door_out);}
   );
 }
 
-date_data.text('%d/%m %a'); time_data.text('%H:%M');
-
-refresh_sensors();
-
-sens_data.borderColor('black'); time_data.borderColor('black'); date_data.borderColor('black');
-
-wind.add(sens_data); wind.add(time_data); wind.add(date_data);
-
-
 wind.show();
 
+refresh_sensors();
