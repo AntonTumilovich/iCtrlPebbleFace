5ec310ac62e65a0c17c4fc9bdd4f0bcb3827ff69
Anton Tumilovich <anton_tumilovich@nevicom.ru>
2014-09-29 12:25:21 +0400

    Few changes

--- src/app.js
+++ src/app.js
@@ -24,21 +24,14 @@ function refresh_sensors(e)
     function(data) 
     {
       setTimeout(function() {
-      sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + "/" + data.door_out + "\nRefresh   : " + hours + ":" + minutes + ".");
+      sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + " / " + data.door_out + "\nRefresh   : " + hours + ":" + minutes + ".");
       }, 6000);
-      sens_data.text("Room : " + data.room_l + "\nHall : " + data.hall_l + "\nToal : " + data.toal_l + "\nBath L/F : " + data.bath_l + "/" + data.bath_f + "\nKitch L/F: " + data.kitch_l + "/" + data.kitch_f);
+      sens_data.text("Room        : " + data.room_l + "\nHall            : " + data.hall_l + "\nToal           : " + data.toal_l + "\nBath L/F  : " + data.bath_l + " / " + data.bath_f + "\nKitch L/F : " + data.kitch_l + " / " + data.kitch_f);
     },
     function() {ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json' }, function(data) {sens_data.text("Room       : " + data.room_th + "\nOut            : " + data.out_th + "\nBath         : " + data.bath_th + "\nKitchen    : " + data.kitch_th + "\nDoor I/O  : " + data.door_in + "/" + data.door_out + "\nRefresh   : " + hours + ":" + minutes);});}
   );
 }
 
 
-
-setTimeout(function() {
-  // Display the mainScreen
-sens_data.text
-}, 400);
-
-
-wind.show(); refresh_sensors();
+wind.show(); wind.fullscreen(); refresh_sensors();
 
